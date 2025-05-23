1cc883264891c4fc77feebc2bb83445dc60de3b4
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-23 16:03:58 -0300

    refactor(webhandler): Remove example usage code from WebHandler class

--- src/webhandler.py
+++ src/webhandler.py
@@ -53,13 +53,3 @@ class WebHandler:
 			return self.driver.find_element(by, value)
 		else:
 			raise Exception("Driver not started. Call start_driver() first.")
-
-
-if __name__ == "__main__":
-	# Example usage
-	driver_path = os.path.join(BASE_PATH, "chromedriver-win64", "chromedriver.exe")
-	web_handler = WebHandler(driver_path)
-	web_handler.navigate_to_url("http://example.com")
-	element = web_handler.find_element(By.TAG_NAME, "h1")
-	print(element.text)
-	del web_handler
