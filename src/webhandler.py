7585f19f7f1c5d058e460e1825055c2b32ab11f6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-23 13:48:48 -0300

    feat(webhandler): Implement WebHandler class for Selenium web interactions

---
+++ src/webhandler.py
@@ -0,0 +1,65 @@
+import os
+from urllib.parse import ParseResult as URL
+from selenium.webdriver.remote.webdriver import WebDriver as Remote
+from selenium.webdriver.remote.client_config import ClientConfig
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from constants import BASE_PATH, SERVICE_PORT, DEFAULT_TIMEOUT
+
+class WebHandler:
+	''' Class to handle web interactions using Selenium. '''
+	service: Service = None
+	driver: Remote
+	service_url = "http://localhost:{}".format(SERVICE_PORT)
+	data_folder = os.path.join(BASE_PATH, "www")
+	def __init__(self, driver_path):
+		# Set up the Service
+		self.service = Service(driver_path)
+		self.service.port = SERVICE_PORT
+		self.service.log_path = None
+		self.service.start()
+		# Set up the Options
+		self.options = Options()
+		self.options.add_argument("--user-data-dir={}".format(self.data_folder))
+		self.options.add_argument("--app={}".format(self.service_url))
+
+		# Set up the ClientConfig
+		self.client_config = ClientConfig(self.service_url)
+		self.client_config.timeout = DEFAULT_TIMEOUT
+
+		# Set up the WebDriver
+		self.driver = Remote(
+			command_executor = self.service_url,
+			options = self.options,
+			client_config = self.client_config
+		)
+	def __del__(self):
+		if self.driver:
+			self.driver.quit()
+			self.driver = None
+		if self.service:
+			self.service.stop()
+			self.service = None
+
+	def navigate_to_url(self, url: URL):
+		if self.driver:
+			self.driver.get(url)
+		else:
+			raise Exception("Driver not started. Call start_driver() first.")
+
+	def find_element(self, by: By, value: str):
+		if self.driver:
+			return self.driver.find_element(by, value)
+		else:
+			raise Exception("Driver not started. Call start_driver() first.")
+
+
+if __name__ == "__main__":
+	# Example usage
+	driver_path = os.path.join(BASE_PATH, "chromedriver-win64", "chromedriver.exe")
+	web_handler = WebHandler(driver_path)
+	web_handler.navigate_to_url("http://example.com")
+	element = web_handler.find_element(By.TAG_NAME, "h1")
+	print(element.text)
+	del web_handler
